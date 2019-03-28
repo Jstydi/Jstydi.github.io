75032370571eeca953ab81d82d8a90c473deec85
Евгений <34836150+Jstydi@users.noreply.github.com>
2019-03-28 22:53:29 +1000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,5 @@
 'use strict';
-self.addEventListener('fetch', (event) => {
-    console.log('Происходит запрос на сервер');
-});
+
 importScripts('sw-toolbox.js');
 toolbox.precache(["index.html"]);
 toolbox.router.get('/images/*', toolbox.cacheFirst);
