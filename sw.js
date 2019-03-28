701fd5109578c9db41a2f6530783d56957c93730
Евгений <34836150+Jstydi@users.noreply.github.com>
2019-03-28 22:51:38 +1000

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,7 +5,8 @@ toolbox.precache(["index.html"]);
 toolbox.router.get('/images/*', toolbox.cacheFirst);
 toolbox.router.get('/*', toolbox.networkFirst, {
 networkTimeoutSeconds: 5
-});
-self.addEventListener('fetch', (event) => {
+    self.addEventListener('fetch', (event) => {
     console.log('Происходит запрос на сервер');
 });
+});
+
