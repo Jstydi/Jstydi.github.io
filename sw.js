f36e0a8187d836cee5fb566ae9934b7106e6e1a7
Евгений <34836150+Jstydi@users.noreply.github.com>
2019-03-28 22:35:30 +1000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,9 @@
 'use strict';
-console.log("Test servis-worcer fail")
+
 importScripts('sw-toolbox.js');
 toolbox.precache(["index.html"]);
 toolbox.router.get('/images/*', toolbox.cacheFirst);
 toolbox.router.get('/*', toolbox.networkFirst, {
 networkTimeoutSeconds: 5
 });
+console.log("Test servis-worcer fail");
