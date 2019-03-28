8322ba96d0dbc97eb88d16f2e03a4d56f9f56d24
Евгений <34836150+Jstydi@users.noreply.github.com>
2019-03-28 22:23:06 +1000

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,5 +4,6 @@ importScripts('sw-toolbox.js');
 toolbox.precache(["index.html"]);
 toolbox.router.get('/images/*', toolbox.cacheFirst);
 toolbox.router.get('/*', toolbox.networkFirst, {
+  console.log("Test servis-worcer fail")
   networkTimeoutSeconds: 5
 });
