09767b8b649ea7d9f473f3165e9e2d56870ef17a
Евгений <34836150+Jstydi@users.noreply.github.com>
2019-03-29 12:52:23 +1000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 'use strict';
 
-setInterval(function() { 
 importScripts('sw-toolbox.js');
+setInterval(function() { 
 toolbox.precache(["index.html"]);
 toolbox.router.get('/images/*', toolbox.cacheFirst);
 toolbox.router.get('/*', toolbox.networkFirst, {
