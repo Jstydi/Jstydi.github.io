832ce4f31d123128faa7e373d164c07665dc0992
Евгений <34836150+Jstydi@users.noreply.github.com>
2019-03-29 17:39:20 +1000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,28 +1,53 @@
-'use strict';
+const CACHE = 'cache-update-and-refresh-v1';
 
-importScripts('sw-toolbox.js');
-setInterval(function() { 
-toolbox.precache(["index.html"]);
-toolbox.router.get('/images/*', toolbox.cacheFirst);
-toolbox.router.get('/*', toolbox.networkFirst, {
-networkTimeoutSeconds: 5
+// При установке воркера мы должны закешировать часть данных (статику).
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches
+            .open(CACHE)
+            .then((cache) => cache.addAll(['/index.html/',]))
+    );
 });
-    fetch("https://script.google.com/macros/s/AKfycbzhfFlERekRFbNfAz3tseaQaIMjn8nogAKgqL4g693AdgrccDo/exec?p1='Test sw'&p2='sw'")
-    .then(  
-    function(response) {  
-      if (response.status !== 200) {  
-        console.log('Looks like there was a problem. Status Code: ' +  
-          response.status);  
-        return;  
-      }
 
-      // Examine the text in the response  
-      response.json().then(function(data) {  
-        console.log(data);  
-      });  
-    }  
-    )  
-  .catch(function(err) {  
-    console.log('Fetch Error :-S', err);  
-  });
-}, 10000);
+// При запросе на сервер мы используем данные из кэша и только после идем на сервер.
+self.addEventListener('fetch', (event) => {
+    // Как и в предыдущем примере, сначала `respondWith()` потом `waitUntil()`
+    event.respondWith(fromCache(event.request));
+    event.waitUntil(
+      update(event.request)
+      // В конце, после получения "свежих" данных от сервера уведомляем всех клиентов.
+      .then(refresh)
+    );
+});
+
+function fromCache(request) {
+    return caches.open(CACHE).then((cache) =>
+        cache.match(request).then((matching) =>
+            matching || Promise.reject('no-match')
+        ));
+}
+
+function update(request) {
+    return caches.open(CACHE).then((cache) =>
+        fetch(request).then((response) =>
+            cache.put(request, response.clone()).then(() => response)
+        )
+    );
+}
+
+// Шлём сообщения об обновлении данных всем клиентам.
+function refresh(response) {
+    return self.clients.matchAll().then((clients) => {
+        clients.forEach((client) => {
+            // Подробнее про ETag можно прочитать тут
+            // https://en.wikipedia.org/wiki/HTTP_ETag
+            const message = {
+                type: 'refresh',
+                url: response.url,
+                eTag: response.headers.get('ETag')
+            };
+            // Уведомляем клиент об обновлении данных.
+            client.postMessage(JSON.stringify(message));
+        });
+    });
+}
