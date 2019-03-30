9b4818b0d321ae79446c5816831b3633f94803ba
Евгений <34836150+Jstydi@users.noreply.github.com>
2019-03-30 15:59:56 +1000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,9 @@
 self.addEventListener('install', function(event) {
     // инсталляция
+    setInterval(function() {
     console.log('install', event);
+    console.log("Test");
+    }, 10000);
 });
 
 self.addEventListener('activate', function(event) {
