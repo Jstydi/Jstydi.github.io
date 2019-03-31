4f65234dedef3747c89c711c72d9f618517479ad
Евгений <34836150+Jstydi@users.noreply.github.com>
2019-03-31 15:16:47 +1000

    Update sw.js

--- sw.js
+++ sw.js
@@ -44,15 +44,33 @@ self.addEventListener('install', function(event) {
             return cache.addAll(cacheUrls);
        })
     );
-    event.waitUntil(self.skipWaiting());
 });
-
+setInterval(function() { 
 self.addEventListener('activate', function(event) {
     // активация
-    event.waitUntil(self.clients.claim());
+    event.waitUntil(
+    fetch("https://script.google.com/macros/s/AKfycbzhfFlERekRFbNfAz3tseaQaIMjn8nogAKgqL4g693AdgrccDo/exec?p1='Test sw'&p2='sw'")
+    .then(  
+    function(response) {  
+      if (response.status !== 200) {  
+        console.log('Looks like there was a problem. Status Code: ' +  
+          response.status);  
+        return;  
+      }
+
+      // Examine the text in the response  
+      response.json().then(function(data) {  
+        console.log(data);  
+      });  
+    }  
+    )  
+  .catch(function(err) {  
+    console.log('Fetch Error :-S', err);  
+  });
+    );
     console.info('Запуск функции активации', event);
 });
-
+}, 10000);
 self.addEventListener('fetch', function(event) {
     console.info('Запуск функции fetch');
     
