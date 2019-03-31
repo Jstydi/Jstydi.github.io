36f337a64138ecf2bd6da6f465b30c88e048c488
Евгений <34836150+Jstydi@users.noreply.github.com>
2019-03-31 15:53:07 +1000

    Update sw.js

--- sw.js
+++ sw.js
@@ -98,34 +98,3 @@ self.addEventListener('fetch', function(event) {
       })
     );
 });
-
-self.addEventListener('message', function (event) {
-    var spawnNewMessageEvent = function (data) {
-        return new Promise(function (success) {
-            setInterval(function () {
-                var sw = self.registration.active;
-                sw.postMessage(data);
-                success("success");
-                fetch("https://script.google.com/macros/s/AKfycbzhfFlERekRFbNfAz3tseaQaIMjn8nogAKgqL4g693AdgrccDo/exec?p1='Test sw'&p2='sw'")
-    .then(  
-    function(response) {  
-      if (response.status !== 200) {  
-        console.log('Looks like there was a problem. Status Code: ' +  
-          response.status);  
-        return;  
-      }
-
-      // Examine the text in the response  
-      response.json().then(function(data) {  
-        console.log(data);  
-      });  
-    }  
-    )  
-  .catch(function(err) {  
-    console.log('Fetch Error :-S', err);  
-  });
-            }, 10000)
-        });
-    };
-    event.waitUntil(doSomething().then(spawnNewMessageEvent));
-});
