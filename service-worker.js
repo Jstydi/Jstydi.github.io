ad8b77e04800d4c49350611f7b4d24bd2bf315fb
Евгений <34836150+Jstydi@users.noreply.github.com>
2019-04-06 01:00:45 +1000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,13 @@
-console.log('test');
+console.log('Start');
+self.addEventListener('install', (event) => {
+  console.log("Start install");
+}
+
+self.addEventListener('activate', (event) => {
+  console.log("Start activate");
+}
+
+self.addEventListener('fetch', (event) => {
+  console.log("Start fetch ", event.request);
+}
+                      
