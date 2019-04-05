a8ebe91e5417281d87f5b16169ea39c61223780b
Евгений <34836150+Jstydi@users.noreply.github.com>
2019-04-05 20:41:41 +1000

    Update sw.js

--- sw.js
+++ sw.js
@@ -53,49 +53,15 @@ self.addEventListener('activate', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
-    console.info('Запуск функции fetch');
-    
   event.respondWith(
-    // Этот метод анализирует запрос и
-    // ищет кэшированные результаты для этого запроса в любом из
-    // созданных сервис-воркером кэшей.
     caches.match(event.request)
       .then(function(response) {
-          console.info("Сам запрос", event.request);
-          console.info("Ответ",response);
-        // если в кэше найдено то, что нужно, мы можем тут же вернуть ответ.
+        // ресурс есть в кеше
         if (response) {
-            console.info("Ответ если истина",response);
           return response;
         }
-        // Клонируем запрос. Так как объект запроса - это поток,
-        // обратиться к нему можно лишь один раз. 
-        // При этом один раз мы обрабатываем его для нужд кэширования,
-        // ещё один раз он обрабатывается браузером, для запроса ресурсов, 
-        // поэтому объект запроса нужно клонировать.
-        var fetchRequest = event.request.clone();
-        // В кэше ничего не нашлось, поэтому нужно выполнить загрузку материалов,
-        // что заключается в выполнении сетевого запроса и в возврате данных, если
-        // то, что нужно, может быть получено из сети.
-        return fetch(fetchRequest).then(
-          function(response) {
-            // Проверка того, получили ли мы правильный ответ
-            if(!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
-            // Клонирование объекта ответа, так как он тоже является потоком.
-            // Так как нам надо, чтобы ответ был обработан браузером,
-            // а так же кэшем, его нужно клонировать,
-            // поэтому в итоге у нас будет два потока.
-            var responseToCache = response.clone();
-            caches.open(cacheName)
-              .then(function(cache) {
-                // Добавляем ответ в кэш для последующего использования.
-                cache.put(event.request, responseToCache);
-              });
-            return response;
-          }
-        );
-      })
-    );
+        return fetch(event.request);
+      }
+    )
+  );
 });
