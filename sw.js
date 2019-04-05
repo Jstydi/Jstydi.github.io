ca2d8c721483226a4f235bfe225cc5ebfc88abdb
Евгений <34836150+Jstydi@users.noreply.github.com>
2019-04-05 20:45:55 +1000

    Update sw.js

--- sw.js
+++ sw.js
@@ -53,6 +53,7 @@ self.addEventListener('activate', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
+    console.log("Запрос ", event.request)
   event.respondWith(
     caches.match(event.request)
       .then(function(response) {
