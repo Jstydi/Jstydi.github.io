c9b047bbaa52b045d68587fed8b382dd483e9a64
Евгений <34836150+Jstydi@users.noreply.github.com>
2019-04-06 00:20:37 +1000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,9 +2,13 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.1.1/workbox
 workbox.setConfig({
   debug: true
 });
-
 if (workbox) {
   console.log(`Yay! Workbox is loaded 🎉`);
 } else {
   console.log(`Boo! Workbox didn't load 😬`);
 }
+
+workbox.routing.registerRoute(
+  new RegExp('.*\.js'),
+  new workbox.strategies.NetworkFirst()
+);
