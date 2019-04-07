e563abeee725048de1fdf4f58d15ef42ce65ad4f
Евгений <34836150+Jstydi@users.noreply.github.com>
2019-04-07 21:11:55 +1000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -48,43 +48,5 @@ self.addEventListener('activate', (event) => {
 // При запросе на сервер мы используем данные из кэша и только после идем на сервер.
 self.addEventListener('fetch', (event) => {
     console.info('Start fetch ', event.request);
-    // Как и в предыдущем примере, сначала `respondWith()` потом `waitUntil()`
-    event.respondWith(fromCache(event.request));
-    event.waitUntil(
-      update(event.request)
-      // В конце, после получения "свежих" данных от сервера уведомляем всех клиентов.
-      .then(refresh)
-    );
+    event.respondWith(caches.match(event.request));
 });
-
-function fromCache(request) {
-    return caches.open(cacheName).then((cache) =>
-        cache.match(request).then((matching) =>
-            matching || Promise.reject('no-match')
-        ));
-}
-
-function update(request) {
-    return caches.open(cacheName).then((cache) =>
-        fetch(request).then((response) =>
-            cache.put(request, response.clone()).then(() => response)
-        )
-    );
-}
-
-// Шлём сообщения об обновлении данных всем клиентам.
-function refresh(response) {
-    return self.clients.matchAll().then((clients) => {
-        clients.forEach((client) => {
-            // Подробнее про ETag можно прочитать тут
-            // https://en.wikipedia.org/wiki/HTTP_ETag
-            const message = {
-                type: 'refresh',
-                url: response.url,
-                eTag: response.headers.get('ETag')
-            };
-            // Уведомляем клиент об обновлении данных.
-            client.postMessage(JSON.stringify(message));
-        });
-    });
-}
