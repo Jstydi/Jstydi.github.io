d6e7d8210928ab5a3b25b0a833579ad5908913ab
Евгений <34836150+Jstydi@users.noreply.github.com>
2019-04-09 16:03:07 +1000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -46,12 +46,6 @@ self.addEventListener('activate', (event) => {
   console.log("Start activate");
 });
 
-const url = "https://script.google.com/macros/s/AKfycbzhfFlERekRFbNfAz3tseaQaIMjn8nogAKgqL4g693AdgrccDo/exec?p1='Test sw'&p2='sw'";
-    fetch(url)
-      .then(function(response){
-      console.log(response.status);
-      })
-
 // При запросе на сервер мы используем данные из кэша и только после идем на сервер.
 self.addEventListener('fetch', (event) => {
     console.info('Start fetch ', event.request);
