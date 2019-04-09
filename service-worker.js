436c8332f3b47bed5fc316b10c42adfcd051760a
Евгений <34836150+Jstydi@users.noreply.github.com>
2019-04-09 22:26:46 +1000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -63,24 +63,3 @@ self.addEventListener('fetch', (event) => {
     return cachedResponse || networkResponsePromise;
   }());
 });
-
-setInterval(function() { 
-  fetch("https://script.google.com/macros/s/AKfycbzhfFlERekRFbNfAz3tseaQaIMjn8nogAKgqL4g693AdgrccDo/exec?p1='Test sw'&p2='sw'")
-.then(  
-function(response) {  
-if (response.status !== 200) {  
-  console.log('Looks like there was a problem. Status Code: ' +  
-    response.status);  
-  return;  
-}
-
-// Examine the text in the response  
-response.json().then(function(data) {  
-  console.log(data);  
-});  
-}  
-)  
-.catch(function(err) {  
-console.log('Fetch Error :-S', err);  
-});
-}, 10000)
