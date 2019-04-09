0ea1f15c8b62166c9eaf9260df7d5260d9dd9229
Евгений <34836150+Jstydi@users.noreply.github.com>
2019-04-09 22:17:14 +1000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -48,6 +48,7 @@ self.addEventListener('activate', (event) => {
 
 // При запросе на сервер мы используем данные из кэша и только после идем на сервер.
 self.addEventListener('fetch', (event) => {
+  console.log('Start fetch ', event.request);
   event.respondWith(async function() {
     const cache = await caches.open(cacheName);
     const cachedResponse = await cache.match(event.request);
