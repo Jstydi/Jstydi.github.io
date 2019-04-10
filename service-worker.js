baf40c4419baa38147ad563d998083b43a64cae7
Евгений <34836150+Jstydi@users.noreply.github.com>
2019-04-10 14:29:06 +1000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -50,8 +50,8 @@ self.addEventListener('activate', (event) => {
 self.addEventListener('fetch', (event) => {
   
   event.respondWith(
-                   caches.match(event.request)
                    console.log('Start fetch ', event.request);
+                   caches.match(event.request)        
   );
-  
+ 
 });
