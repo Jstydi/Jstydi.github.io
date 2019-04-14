8a85cbc85ed8a5d7ee76fb28b5bf137550b2a505
Евгений <34836150+Jstydi@users.noreply.github.com>
2019-04-15 01:40:40 +1000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -95,7 +95,7 @@ function Message() { // В-1
   self.addEventListener('message', function(event){
     if(event.data == "UPDATE"){
       console.log("В сервис воркер сработал updata " ,event.data);
-      return skipWaiting();
+      return self.skipWaiting();
     } else {
     event.ports[0].postMessage({'test': 'This is my response.'});
     }
