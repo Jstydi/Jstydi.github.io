d55fc962d334e2f6f382a21ebc3e112e1a271164
Евгений <34836150+Jstydi@users.noreply.github.com>
2019-04-15 22:40:44 +1000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -96,7 +96,7 @@ function Message() { // В-1
 
 setInterval(function() {  // Запуск функции на отправку сообщений с интервалом 20 сек.
 
-    fetch("https://jstydi.github.io/service-worker.js")
+    fetch("https://jstydi.github.io/service-worker.js",{mode: 'If-None-Match'})
             .then(function(response) {  
                  console.log('Статус ответа : ' + response.status);  
                  return;  
