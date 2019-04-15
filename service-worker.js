6ba6c53255db36089ee594bc39607913fb12fd07
Евгений <34836150+Jstydi@users.noreply.github.com>
2019-04-15 13:30:38 +1000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -94,7 +94,7 @@ function Message() { // В-1
 // ----------------------------------------------------------- // Сообщение В-2
   self.addEventListener('message', function(event){
     if(event.data == "UPDATE"){
-      console.log("В сервис воркер сработал updata " ,event.data);
+      console.log("В сервис воркер сработал updata : " ,event.data);
       clients.claim();
     } else {
     event.ports[0].postMessage({'test': 'This is my response.'});
