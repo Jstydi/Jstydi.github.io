bc8c826169b6a3493cd83ef9da957ebcd737baf8
Евгений <34836150+Jstydi@users.noreply.github.com>
2019-04-19 21:38:20 +1000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -134,19 +134,20 @@ function compareCache(data, url) {
 
 
             function comparisonResult(fetchArr, cacheArr) {
-            
-              for (var i = 0; i < fetchArr[0][1].length; i++){
-                if(fetchArr[0][1][i] === cacheArr[0][1][i]){
-                    console.log(fetchArr[0][1][i])
-                    console.log(cacheArr[0][1][i])
-                    //console.log(i)
-                } else {
-                console.log('Не совпадение',fetchArr[0][0][i], fetchArr[0][1][i])
+            var resObj = {};
+                 for (var i = 0; i < fetchArr[0][1].length; i++){
+                    if(fetchArr[0][1][i] === cacheArr[0][1][i]){
+                 } else {
+                    console.log('Не совпадение',fetchArr[0][0][i], fetchArr[0][1][i])
+                    var prop = fetchArr[0][0][i];
+                    var val = fetchArr[0][1][i];
+                    resObj[prop] = val;
+                  }
                 }
-              }
-            }
-            
-            comparisonResult(fetchArr, cacheArr);
+               return resObj;
+             }
+            var returnCompare = comparisonResult(fetchArr, cacheArr);
+            console.log(returnCompare);
         })
 
     });
