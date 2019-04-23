ab61b762f6e50383908b373b6f8a8a43afa47939
Евгений <34836150+Jstydi@users.noreply.github.com>
2019-04-23 18:45:29 +1000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -166,16 +166,16 @@ function commandDistributor() {
     connection().then(connectresults => {
         if (connectresults.connect == false) {
             console.log('Сеть недоступна ', connectresults);
-        } else if (connectresults`.${service_worker.js}` == true) {
-            console.log("Полученные данные ", connectresults[service_worker.js]);
+        } else if (connectresults['service_worker.js'] == true) {
+            console.log("Полученные данные ", connectresults['service_worker.js']);
             var t1 = performance.now(); // Конец времени выполнения
             console.log((t1 - t0) + " ms "); // Результат 
-        } else if (connectresults.index.html == true) {
-            console.log("Полученные данные ", connectresults.index.html);
+        } else if (connectresults['index.html'] == true) {
+            console.log("Полученные данные ", connectresults['index.html']);
             var t1 = performance.now(); // Конец времени выполнения
             console.log((t1 - t0) + " ms "); // Результат
-        } else if (connectresults.content.json == true) {
-            console.log("Полученные данные ", connectresults.content.json);
+        } else if (connectresults['content.json'] == true) {
+            console.log("Полученные данные ", connectresults['content.json']);
             var t1 = performance.now(); // Конец времени выполнения
             console.log((t1 - t0) + " ms "); // Результат
         } else {
